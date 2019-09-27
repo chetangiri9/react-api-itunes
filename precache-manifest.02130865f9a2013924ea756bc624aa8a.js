ba55eaa9f699725b67e4407bfdf64af593bb9782
“chetangiri9” <chetangiri9@gmail.com>
2019-09-27 19:26:12 +0530

    Updates

---
+++ precache-manifest.02130865f9a2013924ea756bc624aa8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2833c90c5b336d6f7e24e42689651a0",
+    "url": "chetangiri9.github.io/react-api-itunes/index.html"
+  },
+  {
+    "revision": "da967d76c8875bdc8aa9",
+    "url": "chetangiri9.github.io/react-api-itunes/static/css/main.1f38774e.chunk.css"
+  },
+  {
+    "revision": "bb3c43cc83263d5aad91",
+    "url": "chetangiri9.github.io/react-api-itunes/static/js/2.ddee8088.chunk.js"
+  },
+  {
+    "revision": "da967d76c8875bdc8aa9",
+    "url": "chetangiri9.github.io/react-api-itunes/static/js/main.ba704052.chunk.js"
+  },
+  {
+    "revision": "0133b77b9e822ae623de",
+    "url": "chetangiri9.github.io/react-api-itunes/static/js/runtime-main.093407c6.js"
+  }
+]);
